68eecb9f7ed4e1041525fffed26d08ea8c2763bb
Ana Carolina K <anacko@gmail.com>
2021-09-27 17:36:31 -0400

    added form to new url

--- express_server.js
+++ express_server.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const generateRandomString = function() {
+  let source = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let str = '';
+  for (let k = 1; k <= 6; k++) {
+    str += source[Math.floor(Math.random()*source.length)];
+  }
+  return str;
 };
 
 app.get("/", (req, res) => {
@@ -24,6 +35,15 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
